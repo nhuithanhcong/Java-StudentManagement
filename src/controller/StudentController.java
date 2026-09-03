beb7fb9221f24f4be3ac925b7d1a765eb40b5f89
nhuithanhcong <pquan2212@gmail.com>
2026-09-03 23:08:57 +0700

    student controller

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -1,13 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
-/**
- *
- * @author Quan
- */
+import exception.ValidationException;
+import model.Student;
+import repository.StudentRepository;
+import utils.SortingUtils;
+import utils.ValidationUtils;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class StudentController {
+    private final StudentRepository studentRepo;
+    private final List<Student> studentList;
     
+    public StudentController() {
+        this.studentRepo = new StudentRepository();
+        this.studentList = studentRepo.loadAll();
+    }
+    
+    public List<Student> getAllStudents() {
+        return studentList;
+    }
+    
+    public void addStudent(String id, String name, String dob, String email, String facultyId) throws ValidationException {
+        if (id == null || id.trim().isEmpty()) {
+            throw new ValidationException("The student ID must not be blank!");
+        }
+        if (!ValidationUtils.isVEmail(email)) {
+            throw new ValidationException("Invalid email format!");
+        }
+        for (Student s : studentList) {
+            if (s.getId().equalsIgnoreCase(id)) {
+                throw new ValidationException("Student ID already exists!");
+            }
+        }
+        
+        Student student = new Student(id, name, dob, email, facultyId);
+        studentList.add(student);
+        studentRepo.saveAll(studentList);
+    }
+    
+    public Student findById(String id) {
+        for (Student s : studentList) {
+            if (s.getId().equalsIgnoreCase(id)) {
+                return s;
+            }
+        }
+        return null;
+    }
+    
+    public List<Student> searchByName(String keyword) {
+        List<Student> result = new ArrayList();
+        for (Student s : studentList) {
+            if (s.getName().toLowerCase().contains(keyword.toLowerCase())) {
+                result.add(s);
+            }
+        }
+        return result;
+    }
+    public void sortByName() {
+        SortingUtils.sortStudents(studentList, Comparator.comparing(Student::getName));
+    }
 }
--- src/controller/SubjectController.java
+++ src/controller/SubjectController.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
-/**
- *
- * @author Quan
- */
+import exception.ValidationException;
+import model.Subject;
+import repository.SubjectRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubjectController {
+    private final SubjectRepository subjectRepo;
+    private final List<Subject> subjectList;
     
+    public SubjectController() {
+        this.subjectRepo = new SubjectRepository();
+        this.subjectList = subjectRepo.loadAll();
+    }
+    
+    public List<Subject> getSubjects() {
+        return subjectList;
+    }
+    
+    public void addSubject(String subjectId, String subjectName, int) {
+        
+    }
 }
