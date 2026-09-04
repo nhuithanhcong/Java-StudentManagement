33b8e71c617c9a0bfcb46e7d05dbf581c729f831
nhuithanhcong <pquan2212@gmail.com>
2026-09-04 14:12:54 +0700

    SubjectController

--- src/controller/SubjectController.java
+++ src/controller/SubjectController.java
@@ -20,7 +20,30 @@ public class SubjectController {
         return subjectList;
     }
     
-    public void addSubject(String subjectId, String subjectName, int) {
+    public void addSubject(String subjectId, String subjectName, int creadits, List<String> prereqs) throws ValidationException{
+        if (subjectId == null || subjectId.trim().isEmpty()) {
+            thorw new ValidationException("");
+        }
+        if (credits <= 0) {
+            thorw new ValidationException("");
+        }
+        for (Subject s : subjectList) {
+            if (s.getSubjectId().equalsIgnoreCase(subjectId)) {
+                throw new ValidationException("");
+            }
+        }
         
+        Subject subject = new Subject(subjectId, subjectName, creadits, prereqs);
+        subjectList.add(subject);
+        subjectRepo.saveAll(subjectList);
+    }
+    
+    public Subject findById(String subjectId) {
+        for (Subject s : subjectList) {
+            if (s.getSubjectId().equalsIgnoreCase(subjectId)) {
+                return s;
+            }
+        }
+        return null;
     }
 }
